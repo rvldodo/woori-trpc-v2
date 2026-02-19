30abcb4c008672a9646a0cad732253afb38d8025
waterhubdev <developer.waterhub@gmail.com>
2026-02-19 14:09:01 +0700

    feat: add not found section

---
+++ app/[locale]/_sections/not-found-section.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Img from "@/components/html/img";
+import { useTranslations } from "next-intl";
+import notFound from "@/public/assets/ilus/not-found.svg";
+import { Text } from "@/components/html/text";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { PATHS } from "@/app/urls";
+
+export default function NotFoundSection() {
+  const t = useTranslations("NotFound");
+
+  return (
+    <section className="wrapper min-h-screen flex flex-col justify-center items-center">
+      <Img
+        src={notFound}
+        alt="Not found"
+        className="size-96"
+        width={200}
+        height={200}
+      />
+      <div className="flex flex-col justify-center items-center gap-8">
+        <Text variant="display-lg">{t("title")}</Text>
+        <Text variant="body-lg-medium" className="text-center">
+          {t.rich("subtitle", {
+            br: (chunks) => (
+              <>
+                <br />
+                {chunks}
+              </>
+            ),
+          })}
+        </Text>
+        <Link href={PATHS.home.base}>
+          <Button variant="woori" className="p-5 rounded-lg">
+            {t("button")}
+          </Button>
+        </Link>
+      </div>
+    </section>
+  );
+}
--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,43 +1,19 @@
-"use client";
+import NotFoundSection from "./_sections/not-found-section";
 
-import Img from "@/components/html/img";
-import { useTranslations } from "next-intl";
-import notFound from "@/public/assets/ilus/not-found.svg";
-import { Text } from "@/components/html/text";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { PATHS } from "../urls";
+import { getMetadata } from "@/app/metadata";
+import { Metadata } from "next";
+
+export const generateMetadata = async (
+  props: PageProps<"/[locale]">,
+): Promise<Metadata> => {
+  const { locale } = await props.params;
+
+  return await getMetadata({
+    locale,
+    title: "404 Error Not Found",
+  });
+};
 
 export default function NotFound() {
-  const t = useTranslations("NotFound");
-
-  return (
-    <section className="wrapper min-h-screen flex flex-col justify-center items-center">
-      <Img
-        src={notFound}
-        alt="Not found"
-        className="size-96"
-        width={200}
-        height={200}
-      />
-      <div className="flex flex-col justify-center items-center gap-8">
-        <Text variant="display-lg">{t("title")}</Text>
-        <Text variant="body-lg-medium" className="text-center">
-          {t.rich("subtitle", {
-            br: (chunks) => (
-              <>
-                <br />
-                {chunks}
-              </>
-            ),
-          })}
-        </Text>
-        <Link href={PATHS.home.base}>
-          <Button variant="woori" className="p-5 rounded-lg">
-            {t("button")}
-          </Button>
-        </Link>
-      </div>
-    </section>
-  );
+  return <NotFoundSection />;
 }
